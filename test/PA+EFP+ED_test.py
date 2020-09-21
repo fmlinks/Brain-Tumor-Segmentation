e872d8c73c01795b28d4542d176fde707cbb0c74
Fengming Lin <1900258258@qq.com>
2020-09-21 17:27:40 +0100

    Update PA+EFP+ED_test.py

--- test/PA+EFP+ED_test.py
+++ test/PA+EFP+ED_test.py
@@ -1,7 +1,5 @@
 import os
-
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 from keras.callbacks import ModelCheckpoint, Callback, LearningRateScheduler
